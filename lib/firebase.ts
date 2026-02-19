05096a41cfa5fe4ff62bc567e1fcdd429f3f8fc3
alopopau-eng <alopopau@gmail.com>
2026-02-19 03:39:45 +0300

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,13 +5,14 @@ import { getDatabase } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAA1FtUW54Hp1F74uLdwKUPEe3_PVRFobU",
-  authDomain: "zain-8fa09.firebaseapp.com",
-  databaseURL: "https://zain-8fa09-default-rtdb.firebaseio.com",
-  projectId: "zain-8fa09",
-  storageBucket: "zain-8fa09.firebasestorage.app",
-  messagingSenderId: "617837090662",
-  appId: "1:617837090662:web:81421024d76054923c9aa7",
+    apiKey: "AIzaSyD7ssn_oN39OmQcyDlfmpzHMMQZnFyxs4w",
+  authDomain: "jhoirr.firebaseapp.com",
+  databaseURL: "https://jhoirr-default-rtdb.firebaseio.com",
+  projectId: "jhoirr",
+  storageBucket: "jhoirr.firebasestorage.app",
+  messagingSenderId: "107584151799",
+  appId: "1:107584151799:web:5a4f7ddf0653624b9133f2",
+  measurementId: "G-3F1GZ6Q78W"
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
